4b3901ebe97d1633800d9bcf3b9f1ce92f5195dc
unknown <pedros4ps15@gmail.com>
2025-07-13 17:16:21 -0600

    rules page added

--- main_menu.gd
+++ main_menu.gd
@@ -1,9 +1,11 @@
 extends Control
+@onready var node_2d: Node2D = $Node2D
+@onready var v_box_container: VBoxContainer = $VBoxContainer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	node_2d.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,9 +18,17 @@ func _on_start_pressed() -> void:
 
 
 func _on_rules_pressed() -> void:
-	print("Rules pressed")
+	if node_2d.visible == false:
+		node_2d.visible = true
+		v_box_container.visible = false
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 	
+
+
+func _on_rules_back_pressed() -> void:
+	if node_2d.visible == true:
+		node_2d.visible = false
+		v_box_container.visible = true
